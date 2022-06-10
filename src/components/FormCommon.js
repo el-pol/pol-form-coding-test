a123eb7ed26b00c780d5061bac7e807a53115926
Pol Milian <pol@vidjet.io>
2022-06-10 12:36:02 +0200

    logging result (requirement)

--- src/components/FormCommon.js
+++ src/components/FormCommon.js
@@ -77,7 +77,7 @@ const FormCommon = () => {
       validate={handleValidation}
       onSubmit={(values, actions) => {
         setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
+          console.log(JSON.stringify(values, null, 2));
           actions.setSubmitting(false);
         }, 1000);
       }}
